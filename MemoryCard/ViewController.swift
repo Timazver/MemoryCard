0079108ddb45f11b1f33c612d0e3b57aa1204556
Timur <tima.yegemberdiev@gmail.com>
2020-03-11 12:06:22 +0600

    Initial Commit

---
+++ MemoryCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryCard
+//
+//  Created by tima on 3/11/20.
+//  Copyright © 2020 MemoryCard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
